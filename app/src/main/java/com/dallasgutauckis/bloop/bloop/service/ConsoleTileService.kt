2e5c755ddc29ae61099d5b391dc43614b671b6cf
Dallas Gutauckis <dallas@gutauckis.com>
2019-03-21 14:12:46 -0400

    Try to start activity from settings tile

--- app/src/main/java/com/dallasgutauckis/bloop/bloop/service/ConsoleTileService.kt
+++ app/src/main/java/com/dallasgutauckis/bloop/bloop/service/ConsoleTileService.kt
@@ -1,24 +1,20 @@
 package com.dallasgutauckis.bloop.bloop.service
 
 import android.annotation.TargetApi
+import android.content.Intent
 import android.os.Build
 import android.service.quicksettings.TileService
 import android.util.Log
+import com.dallasgutauckis.bloop.bloop.ExternalAppConfigurationActivity
 
 @TargetApi(Build.VERSION_CODES.N)
 class ConsoleTileService : TileService() {
-    // TODO this tile isn't appearing; find out why
-
     private val TAG = this.javaClass.simpleName
-
-    override fun onCreate() {
-        Log.v(TAG, "onCreate")
-        super.onCreate()
-    }
 
     override fun onClick() {
         Log.v(TAG, "Tile clicked")
-        qsTile.label = "${qsTile.label}a"
-        qsTile.updateTile()
+        // qsTile.label = "${qsTile.label}a"
+        // qsTile.updateTile()
+        startActivity(Intent(applicationContext, ExternalAppConfigurationActivity::class.java))
     }
 }
